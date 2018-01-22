d47f46e044824218a949491c2922be00bf9497da
Technologies Efforsys <info@efforsys.com>
2018-01-22 13:50:09 +0000

    login, logout, and splash-screen implementation

--- webapp/controller/MainPage.controller.js
+++ webapp/controller/MainPage.controller.js
@@ -1,9 +1,15 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller"
 ], function(Controller) {
-	"use strict";
 
 	return Controller.extend("OptarosSAPUI5POC.controller.MainPage", {
-
+		
+		onInit : function(){
+			busyDialog.close();
+		},
+		
+        onPressLogout : function(){
+            window.location.reload(true);
+        }
 	});
 });
